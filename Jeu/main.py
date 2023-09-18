53d1b45cdf32bfc8ae21a3a34790b7d612dadbc4
Virgile73 <124370671+Virgile73@users.noreply.github.com>
2023-09-18 16:27:21 +0200

    platform v0
    platform v0

--- Jeu/main.py
+++ Jeu/main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from player import Player
+from platform import Platform
 
 # Initialisation de Pygame
 pygame.init()
@@ -19,6 +20,8 @@ pygame.display.set_caption("Jeu de Plateforme")
 
 # Création des groupes de sprites
 all_sprites = pygame.sprite.Group()
+platform1 = Platform(100, 500, 200, 20)  # Example platform
+all_sprites.add(platform1)
 player = Player()
 all_sprites.add(player)
 
@@ -28,6 +31,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    collide_list = pygame.sprite.spritecollide(player, all_sprites, False)
+    if collide_list:
+    # Player is on a platform, handle jumping and gravity accordingly
+        player.is_jumping = False
+        player.rect.y = collide_list[0].rect.top  # Place player on top of the platform
+    # Player is in the air, apply gravity or allow jumping
+
 
     # Mise à jour des sprites
     all_sprites.update()
---
+++ Jeu/platform.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Platform(pygame.sprite.Sprite):
+    
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill((0, 255, 0))  # Green color for the platform, you can use your own image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
