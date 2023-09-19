6bcb233754ffae3508ffea9235289c8c855d24e5
Virgile73 <124370671+Virgile73@users.noreply.github.com>
2023-09-19 17:04:29 +0200

    Efface code inutile

--- Jeu/mainvirgile.py
+++ Jeu/mainvirgile.py
@@ -47,59 +47,8 @@ while starting:
         sys.exit()
     elif event.type == KEYDOWN:
         if event.key == K_SPACE:
-            starting=False
-# Classe de la camera
-class Camera(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-        self.display_surface = pygame.display.get_surface()
+            starting=False"""
 
-        # camera offset
-        self.offset = pygame.math.Vector2()
-        self.half_w = self.display_surface.get_size()[0]//2
-        self.half_h = self.display_surface.get_size()[1]//2
-
-        # box setup
-        self.camera_borders = {'left':200,'right':200,'top':100,'bottom':100}
-        l = self.camera_borders['left']
-        t = self.camera_borders['top']
-        w = self.display_surface.get_size()[0] -(self.camera_borders['left']+self.camera_borders['right'])
-        h = self.display_surface.get_size()[1] -(self.camera_borders['top']+self.camera_borders['bottom'])
-        self.camera_rect = pygame.Rect(l,t,w,h)
-
-        # ground
-        self.ground_surf = pygame.image.load('./assets/backgrounds/niveau1.png').convert_alpha()
-        self.fround_rect = self.ground_surf.get_rect(topleft  =(0,0))
-
-    def center_target_camera(self,target):
-        a=1
-
-    def box_target_camera(self,target):
-
-        if target.rect.left < self.camera_rect.left:
-            self.camera_rect.left = target.rect.left
-        if target.rect.right < self.camera_rect.right:
-            self.camera_rect.right = target.rect.right
-
-
-        self.offset.x = self.camera_rect.left - self.camera_borders['left']
-        self.offset.y = self.camera_rect.top - self.camera_borders['top']
-
-
-    def custom_draw(self):
-
-        # self.center_target_camera(player)
-
-        # ground
-        ground_offset = self.ground_rect.topleft + self.offset
-        self.display_surface.blit(self.ground_surf,self.ground_rect)
-
-        # active elements  
-        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft + self.offset
-            self.display_surface.blit(sprite.image,offset_pos)  
-
-        pygame.draw.rect(self.display_surface,'yellow',self.camera_rect, 5)"""
 
 # Classe du joueur
 
