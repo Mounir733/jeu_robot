67732626d2ba272d8d86fff1fcc9b383a10fe143
MaxenceLucas <maxencelucasmartin38@gmail.com>
2023-09-20 08:53:30 +0200

    magnetic object

--- Jeu/Classes/InteractiveObject/magneticObject.py
+++ Jeu/Classes/InteractiveObject/magneticObject.py
@@ -1,41 +1,84 @@
 import pygame
+from pygame.math import Vector2
 from Classes.InteractiveObject.interactiveObject import InteractiveObject
-
+""""
 class MagneticObject(InteractiveObject):
-    def __init__(self, x, y, sprite_image, magnetic_type, field_range, force, field_directions=None):
+    def __init__(self, x, y, sprite_image, magnetic_type, field_range, force, field_directions=["right"]):
         super().__init__(x, y, sprite_image)
         self.magnetic_type = magnetic_type  # Type d'aimant (1 pour attractif, -1 pour répulsif)
         self.field_range = field_range  # Portée d'attraction
         self.force = force  # Force de champ magnétique
-        self.field_directions = field_directions or []  # Liste des directions du champ de force
+        self.field_directions = field_directions  # Liste des directions du champ de force
+
+    def calculate_field_zone(self, direction):
+        object_center = Vector2(self.rect.center)
+
+        # Calculez la zone du champ de force en fonction de la direction spécifiée
+        if direction == "right":
+            return pygame.Rect(object_center.x, object_center.y - self.rect.height / 2, self.field_range, self.rect.height)
+        elif direction == "left":
+            return pygame.Rect(object_center.x - self.field_range, object_center.y - self.rect.height / 2, self.field_range, self.rect.height)
+        elif direction == "up":
+            return pygame.Rect(object_center.x - self.rect.width / 2, object_center.y - self.field_range, self.rect.width, self.field_range)
+        elif direction == "down":
+            return pygame.Rect(object_center.x - self.rect.width / 2, object_center.y, self.rect.width, self.field_range)
 
     def interact(self, player):
-        dx = self.rect.centerx - player.rect.centerx
-        dy = self.rect.centery - player.rect.centery
+        player_center = Vector2(player.rect.center)
 
-        force_x = 0
-        force_y = 0
+        for direction in self.field_directions:
+            # Vérifiez si la direction spécifiée est présente dans la liste des directions
+            if direction in ["right", "left", "up", "down"]:
+                # Calculez la zone du champ de force
+                field_zone = self.calculate_field_zone(direction)
 
-        distance = max(1, (dx ** 2 + dy ** 2) ** 0.5)  # Évite la division par zéro
+                # Vérifiez si la zone est valide
+                if field_zone.width > 0 and field_zone.height > 0:
+                    # Vérifiez si le joueur se trouve dans la zone du champ de force
+                    if field_zone.collidepoint(player_center):
+                        # Calculez le vecteur entre le joueur et l'objet
+                        direction_vector = player_center - Vector2(self.rect.center)
+                        direction_vector.normalize_ip()
 
-        dansLargeurObjet = False
-        if self.rect.top <= player.rect.centery <= self.rect.bottom:
-            dansLargeurObjet = True
-        else:
-            dansLargeurObjet = False
+                        # Appliquez la force magnétique en fonction de magnetic_type
+                        force_vector = direction_vector * self.force * self.magnetic_type
 
-        if distance < self.field_range:
-            if "left" in self.field_directions and dansLargeurObjet:
-                force_x -= self.magnetic_type * self.force
-            if "right" in self.field_directions and dansLargeurObjet:
-                force_x += self.magnetic_type * self.force
-            if "up" in self.field_directions:
-                force_y -= self.magnetic_type * self.force
-            if "down" in self.field_directions:
-                force_y += self.magnetic_type * self.force
+                        # Appliquez la force au joueur en ajustant directement sa position
+                        player.rect.center += force_vector
+"""
 
-            player.rect.x += force_x
-            player.rect.y += force_y    
+class MagneticObject(InteractiveObject):
+    def __init__(self, x, y, sprite_image, magnetic_type, field_range, force, field_directions=["right"]):
+        super().__init__(x, y, sprite_image)
+        self.magnetic_type = magnetic_type  # Type d'aimant (1 pour attractif, -1 pour répulsif)
+        self.field_range = field_range  # Portée d'attraction
+        self.force = force  # Force de champ magnétique
+        self.field_directions = field_directions  # Liste des directions du champ de force
 
+    def calculate_field_zone(self, direction):
+        object_center = Vector2(self.rect.center)
+
+        # Calculez la zone du champ de force en fonction de la direction spécifiée
+        if direction == "right":
+            return pygame.Rect(object_center.x, object_center.y - self.rect.height / 2, self.field_range, self.rect.height)
+        elif direction == "left":
+            return pygame.Rect(object_center.x - self.field_range, object_center.y - self.rect.height / 2, self.field_range, self.rect.height)
+        elif direction == "up":
+            return pygame.Rect(object_center.x - self.rect.width / 2, object_center.y - self.field_range, self.rect.width, self.field_range)
+        elif direction == "down":
+            return pygame.Rect(object_center.x - self.rect.width / 2, object_center.y, self.rect.width, self.field_range)
         
-        
+    def interact(self, player): 
+        for direction in self.field_directions:
+            # Calculez la zone du champ de force
+            field_zone = self.calculate_field_zone(direction)
+
+            if field_zone.colliderect(player.rect):
+                if direction == "right":
+                    player.rect.centerx += self.force * self.magnetic_type
+                elif direction == "left":
+                    player.rect.centerx -= self.force * self.magnetic_type
+                elif direction == "up":
+                    player.rect.centery -= self.force * self.magnetic_type
+                elif direction == "down":
+                    player.rect.centery += self.force * self.magnetic_type
