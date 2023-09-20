620991f6c187db30aea3a4df9ef7072cbf4c9ab4
Mounir733 <mounir73300@gmail.com>
2023-09-20 15:31:26 +0200

    ajout start et niveau1

--- Jeu/niveau1.py
+++ Jeu/niveau1.py
@@ -12,50 +12,20 @@ pygame.init()
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Couleurs
+# Variable qui change
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
-# Police de caractères pour le menu
 font = pygame.font.Font(None, 36)
 
-# Créer une fonction pour afficher le menu
-def show_menu():
-    menu = True
-    while menu:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    menu = False  # Lancer le jeu principal lorsque la touche Espace est pressée
-        fond = pygame.image.load("assets/backgrounds/space.jpg")
-
-        screen.blit(fond, (0, 0))
-
-        # Afficher le titre du jeu
-        title_text = font.render("KingOfMagnet", True, WHITE)
-        screen.blit(title_text, (300, 200))
-
-        # Afficher les instructions
-        instructions_text = font.render("Appuyez sur Espace pour commencer", True, WHITE)
-        screen.blit(instructions_text, (250, 300))
-
-        pygame.display.flip()
 
 # Fonction principale du jeu
 def main_game():
-    
-    # Constantes
-    SCREEN_WIDTH = 1024
-    SCREEN_HEIGHT = 768
+
 
     # Création de la fenêtre du jeu
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Jeu de Plateforme")
+    pygame.display.set_caption("Niveau 1")
 
     #cration de la camera
     camera = Camera()
@@ -167,7 +137,7 @@ def main_game():
 
         screen.blit(background_surface, (0, 0), pygame.Rect(background_x, background_y, SCREEN_WIDTH, SCREEN_HEIGHT))
         #dessine le compteur minerai
-        show_ore_screen(screen, pygame.font.Font(None, 36), pygame.image.load("assets/objets_interactibles/Red_crystal1.png"), player.numberOre  )
+        show_ore_screen(screen, font, pygame.image.load("assets/objets_interactibles/Red_crystal1.png"), player.numberOre  )
 
         # Dessinez l'obstacle
         obstacles_group.update()
@@ -199,6 +169,4 @@ def game_over(player):
         print("MORT!")
         return True
 # Boucle de jeu
-show_menu()  # Afficher le menu au démarrage
-while True:
-    main_game()  # Lancer le jeu principal
+
